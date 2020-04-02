8d5e64e22f21c8ebcb795bfc68c52ba008c4c12c
zztianx1x <62972437+zztianx1x@users.noreply.github.com>
2020-04-02 18:55:52 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -16,8 +16,7 @@ jsproxy_config({
       label: '演示服务-香港节点',
       lines: {
         // 主机:权重
-        'node-aliyun-hk-1.etherdream.com:8443': 1,
-        'node-aliyun-hk-2.etherdream.com:8443': 2,
+  
       }
     },
     'demo-sg': {
@@ -42,10 +41,8 @@ jsproxy_config({
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
-        //'b.007.workers.dev': 1,
-        //'b.hehe.workers.dev': 1,
-        //'b.lulu.workers.dev': 1,
-        //'b.jsproxy.workers.dev': 1,
+        'https://still-field-ea7f.tiandi321.workers.dev': 1,
+   
       }
     }
   },
@@ -85,13 +82,7 @@ jsproxy_config({
    * URL 自定义处理（设计中）
    */
   url_handler: {
-    'https://www.baidu.com/img/baidu_resultlogo@2.png': {
-      replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
-    },
-    'https://www.pornhub.com/': {
-      redir: 'https://php.net/'
-    },
-    'http://haha.com/': {
+
       content: 'Hello World'
     },
   }
