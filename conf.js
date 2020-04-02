1a7c08ddb01f7155eefef6939d5c0bb0f63b86b6
zztianx1x <62972437+zztianx1x@users.noreply.github.com>
2020-04-02 19:16:29 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -15,15 +15,15 @@ jsproxy_config({
     'demo-hk': {
       label: '演示服务-香港节点',
       lines: {
-         'https://still-field-ea7f.tiandi321.workers.dev': 1,
         // 主机:权重
-  
+        'still-field-ea7f.tiandi321.workers.dev': 1,
+        'node-aliyun-hk-2.etherdream.com:8443': 2,
       }
     },
     'demo-sg': {
       label: '演示服务-新加坡节点',
       lines: {
-
+        'node-aliyun-sg.etherdream.com:8443': 1,
       },
     },
     'mysite': {
@@ -94,4 +94,3 @@ jsproxy_config({
     },
   }
 })
-
